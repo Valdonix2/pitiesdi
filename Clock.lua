01f32684d06ed30fc68ccbb1b0da6cd2956ea9af
Valdonix2 <123013224+Valdonix2@users.noreply.github.com>
2023-03-05 11:37:42 +0200

    ClockMain
    ClockMain

---
+++ Clock.lua
@@ -0,0 +1,77 @@
+---====== Define spawner ======---
+
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
+
+---====== Create entity ======---
+
+local entity = Spawner.createEntity({
+    CustomName = "Template Entity",
+    Model = "https://github.com/Valdonix2/pitiesdi/raw/main/Clock.rbxm", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
+    Speed = 150,
+    MoveDelay = 10,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    SpawnInFront = true,
+    ShatterLights = false,
+    FlickerLights = {
+        Enabled = true,
+        Duration = 10
+    },
+    Cycles = {
+        Min = 1,
+        Max = 5,
+        Delay = 3
+    },
+    CamShake = {
+        Enabled = true,
+        Values = {1.5, 20, 0.1, 1},
+        Range = 100
+    },
+    ResistCrucifix = false,
+    BreakCrucifix = true,
+    DeathMessage = {"You died of a heart attack...", "Seems like you were scared to death by Clock", "He can spawn whenever a door is opened. And he appears in the next room infront of you.", "He will give you plenty of time to hide!", "Note that it can come back like ambush does!"},
+    IsCuriousLight = false
+})
+
+---====== Debug ======---
+
+entity.Debug.OnEntitySpawned = function()
+    print("Entity has spawned")
+end
+
+entity.Debug.OnEntityDespawned = function()
+    print("Entity has despawned")
+end
+
+entity.Debug.OnEntityStartMoving = function()
+    print("Entity started moving")
+end
+
+entity.Debug.OnEntityFinishedRebound = function()
+    print("Entity finished rebound")
+end
+
+entity.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function()
+    print("Player looking at entity")
+end
+
+entity.Debug.OnDeath = function()
+    print("Player has died")
+end
+
+--[[
+    NOTE: By overwriting 'OnUseCrucifix', the default crucifixion will be ignored and this function will be called instead
+
+    entity.Debug.OnUseCrucifix = function()
+        print("Custom crucifixion script here")
+    end
+]]--
+
+---====== Run entity ======---
+
+Spawner.runEntity(entity)
