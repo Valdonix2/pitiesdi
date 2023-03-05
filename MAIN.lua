9ac0bae2af18f0b2d2567355ffa93511b2094151
Valdonix2 <123013224+Valdonix2@users.noreply.github.com>
2023-03-05 11:44:26 +0200

    MAIN
    MAIN

---
+++ MAIN.lua
@@ -0,0 +1,9 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Valdonix2/pitiesdi/main/OnRun.lua'))()
+print("PTSD mode by Valdonix loaded!")
+
+while true do
+	game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+  	print("DOOR OPENED")
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Valdonix2/pitiesdi/main/ClockSpawn'))()
+	wait(0.5)
+end
