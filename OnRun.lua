27906dfb7e1cf0fa245905fc0e913e2a74e46473
Valdonix2 <123013224+Valdonix2@users.noreply.github.com>
2023-03-05 11:16:37 +0200

    Onrun.lua
    OnRun.lua

---
+++ OnRun.lua
@@ -0,0 +1,8 @@
+local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+Achievements.Get({
+    Title = "Welcome",
+    Desc = "Time to suffer!",
+    Reason = "You have been given PTSD!",
+    Image = "https://download1531.mediafire.com/zczea84u6qugb4lMzOxlsRjmzJzmQPnnjOPH7r8u1AARWhbI-JsqhyFVMIjUkHUZAVz_bEfMOmd77WWahWeWE4wLR0Kwww/wm6y5b5e35lq2xg/epic.png",
+})
